fc7aec114187aab9328c79a76031ebd95ff672b1
juanLinero <juanlinerobonnett611@gmail.com>
2023-01-25 17:20:24 -0500

    full project

---
+++ script.js
@@ -0,0 +1,62 @@
+let arrImg = 
+[
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftwinfinite.net%2Fwp-content%2Fuploads%2F2020%2F08%2FGhostwire-Tokyo.jpg&f=1&nofb=1&ipt=4eaed3e06c1dc13d757c988ce700625dd818265de0f3a2dbd844d3a1333f74d9&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SwdX2pYMOj7axKCEc-P5SgHaDt%26pid%3DApi&f=1&ipt=b2d7fe10ebb901476e57cc1b4c475d3ef79596ca8393894bd89a10325a4fe3ca&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nintendo-insider.com%2Fwp-content%2Fuploads%2F2020%2F05%2Flego_star_wars_the_skywalker_saga_key_art.jpg&f=1&nofb=1&ipt=556b2558dd1c2f87606a3b21a933806e75de8faf70c62d904e827f50f98aadd2&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpopculturetimes.com%2Fwp-content%2Fuploads%2F2020%2F07%2Fmaxresdefault-2-3.jpg&f=1&nofb=1&ipt=ead681b5bdbb9b7bdd41f96f50d04bd4711e371730dd84cbc49e47b2957f48bc&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hutmobile.com%2Fwp-content%2Fuploads%2F2020%2F07%2F1-418-1536x858.jpg&f=1&nofb=1&ipt=7bb8cb2d95a17547e131159721b0da38caff897f74fa248fc3fc033311fe407d&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbleedingcool.com%2Fwp-content%2Fuploads%2F2020%2F03%2Fthe-quarry-1200x628.jpeg&f=1&nofb=1&ipt=9dc0dd45774e56536b294bbf6905afbaa81f56541cb6661eec11ad929bd891d9&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages2.minutemediacdn.com%2Fimage%2Ffetch%2Fw_2000%2Ch_2000%2Cc_fit%2Fhttps%3A%252F%252Fapptrigger.com%252Ffiles%252F2022%252F02%252Fhorizon-forbidden-west-art.jpg&f=1&nofb=1&ipt=248656d6eab38a6971ef68019d983f5f05b3ef2942d87eb108dea8800c12239e&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.ungeek.ph%2Fwp-content%2Fuploads%2F2021%2F02%2Fpokemon_legends_arceus_announcement.jpg&f=1&nofb=1&ipt=8397cf4e19580f63beeace67b99b0f3c27e4fdd1dd8875bef4c315c96118cc0a&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsteamcdn-a.akamaihd.net%2Fsteam%2Fapps%2F553420%2Fheader.jpg%3Ft%3D1530121523&f=1&nofb=1&ipt=e2994fd044746b2b2d7a27256c8fa15aa1ca7af392bb7e9b6f0ddc39ac08bdad&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F2enbYOMtR4I%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=1f5b906509eba0c827d31ec134ef08bf532544e6948da3251f8430f7f64d70c1&ipo=images"
+]
+arrImg.reverse() //escribi mal el orden el array y me da flojera cambiarlo
+
+let arrInfo = 
+[
+    "Elden Ring es un RPG de fantasía oscura que te permite jugar a uno de los descendientes de los Tarnished. Un paria, debes entrar en las Tierras Intermedias para tratar de obtener la readmisión en la sociedad allí como un Señor Elden. Para hacer eso, debes revelar el misterio del legendario Elden Ring. Tus aventuras en las Tierras Intermedias estarán llenas de amenazas y rebosarán de desconocidos.", 
+    "Tunic es un juego de acción y aventura de Andrew Shouldice y publicado por Finji. El juego se lanzó en marzo de 2022 en Microsoft Windows, macOS, Xbox Series X / S y Xbox One, que fue seguido por puertos en Nintendo Switch, PlayStation 4 y PlayStation 5, en septiembre.",
+    "Pokémon Legends: Arceus es un juego de rol de acción que conserva la jugabilidad central de las entradas anteriores en la serie principal. El juego está ambientado en una época pasada de la historia de la región de Sinnoh, cuando era conocida como la región de Hisui, mucho antes de que tuvieran lugar los eventos de Pokémon Diamante y Perla y sus remakes. El Pokémon mítico Arceus juega un papel importante en la historia.",    
+    "Horizon Forbidden West continúa la historia de Aloy medio año después de los eventos de Zero Dawn, una joven cazadora de la tribu Nora enviada en una búsqueda a una misteriosa frontera que abarca Utah hasta la costa del Pacífico para encontrar la fuente de una misteriosa plaga que mata a todo lo que infecta. En su viaje a través de las tierras inexploradas del Oeste Prohibido, se encuentra con regiones hostiles llenas de amenazas naturales y devastadas por tormentas masivas, enemigos peligrosos y máquinas mortales.",
+    "Del 24 al 25 de junio de 2021, Laura y Max, nuevos consejeros en el campamento de verano de Hackett's Quarry, conducen por la ruta al campamento una noche antes de su apertura para ver qué tiene para ofrecer. Tratando de evitar golpear a una criatura desconocida que apareció repentinamente en el camino, los dos se desvían de la ruta y se estrellan contra el bosque. Allí son encontrados por un sheriff del condado local, Travis, que sospecha de su situación.",
+    "Rogue Legacy 2 is a platform game with roguelike and Metroidvania elements. In the game, the player assumes control of a knight, who must explore procedurally generated dungeons to collect its treasures and defeat enemies. While the knight is equipped with swords and shields and mages can cast magic, the game introduces several additional gameplay classes. As a roguelike, players will have to start from the beginning when their player avatar is killed in the game.",
+    "Lost, alone and separated from family, a stray cat must untangle an ancient mystery to escape a long-forgotten city. Stray is a third-person cat adventure game set amidst the detailed, neon-lit alleys of a decaying cybercity and the murky environments of its seedy underbelly. Roam surroundings high and low, defend against unforeseen threats and solve the mysteries of this unwelcoming place inhabited by curious droids and dangerous creatures.",
+    "The galaxy is yours with LEGO Star Wars: The Skywalker Saga! Play through all nine Star Wars saga films in a brand-new LEGO video game unlike any other! Fly through the galaxy to discover the saga’s most legendary locations, and switch between stories at will. From podracing on Tatooine in Star Wars: The Phantom Menace to the epic showdowns of Star Wars: The Rise of Skywalker. Want to play as a Jedi? A Sith? Rebel, bounty hunter, or droid?... it's the ultimate LEGO Star Wars experience.",
+    "Rollerdrome is a single-player third-person action-shooter that seamlessly blends high-octane combat with fluid motion to create an action experience like no other. Dominate with style in cinematic, visceral combat where kills net you health and pulling off tricks and grinds provide you with ammunition.",
+    "Tokyo is overrun by deadly supernatural forces, perpetrated by a dangerous occultist, causing Tokyo’s population to vanish in an instant. Ally with a powerful spectral entity on their quest for vengeance and master a powerful arsenal of abilities to unravel the dark truth behind the disappearance as you face the unknown in Ghostwire: Tokyo."
+]
+
+const IMGCONTAINER = document.querySelector(".img__container")
+const INFO         = document.querySelector(".info__container")
+const INNERINFO    = document.querySelector(".inner__info__container")
+const BTNIZQ       = document.querySelector(".btn__izq")
+const BTNDER       = document.querySelector(".btn__der")
+const NUM          = document.querySelector(".num")
+
+    let n = 0;
+    NUM.textContent = n;
+
+    let nValue = () => {
+        INNERINFO.style = `display: none`
+        IMGCONTAINER.innerHTML = `<img src="${arrImg[n - 1]}"></img>` 
+        NUM.textContent = `🏆${n}🏆`;
+    }
+    BTNIZQ.addEventListener("click", () => {
+        (n <= 1) ? n : n--;
+        return nValue()
+    })
+    BTNDER.addEventListener("click", () => {
+        (n >= 10) ? n : n++
+        return nValue()
+    })
+    IMGCONTAINER.addEventListener("mouseover", () => {
+        IMGCONTAINER.style = `opacity: 0.2`
+        INFO.textContent = arrInfo[n - 1] 
+        INFO.style = `opacity: 1`
+        INFO.style = `transform: translate(-82%)`
+    })
+    IMGCONTAINER.addEventListener("mouseout", () => {
+        IMGCONTAINER.style = `opacity: 1`
+        INFO.style = `opacity: 0`
+    })
